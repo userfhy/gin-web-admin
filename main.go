949e8afe7d18f74b882b03568baa6c25aaed1e0b
fanhengyuan <mrfhy@qq.com>
2019-08-27 15:30:14 +0800

    graceful stop

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+    "context"
     "fmt"
     model "gin-test/app/models"
     "gin-test/routers"
@@ -8,6 +9,10 @@ import (
     "github.com/gin-gonic/gin"
     "log"
     "net/http"
+    "os"
+    "os/signal"
+    "syscall"
+    "time"
 )
 
 func init() {
@@ -39,11 +44,31 @@ func main() {
         MaxHeaderBytes: maxHeaderBytes,
     }
 
-    log.Printf("[info] start http server listening %s", endPoint)
+    go func() {
+        // 服务连接
+        err := server.ListenAndServe()
+        if err != nil {
+            log.Fatalln(err)
+        }
+    }()
 
-    err := server.ListenAndServe()
-    if err != nil {
-        log.Println(err)
-        return
+    log.Printf("[info] start http server listening %s", endPoint)
+    log.Printf("[info] Actual pid is %d", os.Getpid())
+
+    // 等待中断信号以优雅地关闭服务器（设置 5 秒的超时时间）
+    quit := make(chan os.Signal, 1)
+    // kill (no param) default send syscanll.SIGTERM
+    // kill -2 is syscall.SIGINT
+    // kill -9 is syscall. SIGKILL but can"t be catch, so don't need add it
+    signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+    <-quit
+    log.Println("Shutdown Server...")
+
+    ctx, cancel := context.WithTimeout(context.Background(), 5 * time.Second)
+    defer cancel()
+    if err := server.Shutdown(ctx); err != nil {
+        log.Fatal("Server Shutdown: ", err)
     }
+
+    log.Println("Server exiting")
 }
