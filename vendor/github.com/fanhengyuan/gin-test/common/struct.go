9326fc5c3757ceae96a15cf70d4ea69c02a8b255
fanhengyuan <mrfhy@qq.com>
2019-08-30 16:24:38 +0800

    rm vendor

--- vendor/github.com/fanhengyuan/gin-test/common/struct.go
+++
@@ -1,9 +0,0 @@
-package common
-
-type ImgText struct {
-    Text string `json:"text"`
-    X int `json:"x"`
-    Y int `json:"y"`
-    FontSize float64 `json:"fontSize"`
-    Color int `json:"color"`
-}
