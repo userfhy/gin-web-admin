f7c7e1cd17c8a280fcb18a9d4d078d55abd16867
fanhengyuan <mrfhy@qq.com>
2019-09-06 14:10:52 +0800

    Swagger Schemes

--- routers/router.go
+++ routers/router.go
@@ -22,12 +22,13 @@ func InitRouter() *gin.Engine {
     prefixUrl := setting.AppSetting.PrefixUrl
     if strings.HasPrefix(prefixUrl, "http://") {
         configHost = strings.Replace(prefixUrl, "http://", "", -1)
+        docs.SwaggerInfo.Schemes = []string{"http"}
     }else if strings.HasPrefix(prefixUrl, "https://") {
         configHost = strings.Replace(prefixUrl, "https://", "", -1)
+        docs.SwaggerInfo.Schemes = []string{"https"}
     }
     docs.SwaggerInfo.Host = configHost
     docs.SwaggerInfo.BasePath = "/v1/api/"
-    docs.SwaggerInfo.Schemes = []string{"http", "https"}
 
     r := gin.New()
     r.Use(gin.Logger())
