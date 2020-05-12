f745cdcf66672fb2f9203ea6855a08a2dacd59d6
fanhengyuan <fanhengyuan1994@gmail.com>
2020-05-12 23:01:51 +0800

    docker

---
+++ entrypoint.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+#go env -w GOPROXY=https://goproxy.cn,direct \
+#    && go mod download \
+#    && go build main.go \
+#    && ls -al
+
+export GOPROXY=https://goproxy.cn
+
+go mod vendor
+
+go ls -al && run main.go
