d60e23ad10dda39e1eee5d85b12684c9bf4bf903
fanhengyuan <fanhengyuan1994@gmail.com>
2020-05-13 13:59:40 +0800

    validator

--- app/service/v1/report/report_service.go
+++ app/service/v1/report/report_service.go
@@ -7,7 +7,7 @@ import (
 // 上报信息
 type ReportStruct struct {
     Name string `json:"name" form:"name" validate:"required,min=1,max=10" minLength:"1",maxLength:"10"`
-    Phone string `json:"phone" form:"phone" validate:"required,min=4,max=20" minLength:"4",maxLength:"20"`
+    Phone string `json:"phone" form:"phone" validate:"required,numeric,min=4,max=15" minLength:"4",maxLength:"15"`
 }
 
 // 录入信息
